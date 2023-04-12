3bd02447c7358a862a2ddd4482288efac35d78be
Yeska757 <gendosik2@gmail.com>
2023-04-11 21:37:19 -0400

    tuesday commit

---
+++ src/class9HW/newNew.java
@@ -0,0 +1,7 @@
+package class9HW;
+
+public class newNew {
+    public static void main(String[] args) {
+        System.out.println("code");
+    }
+}
--- src/class9HW/newNewHWexGit.java
+++
@@ -1,7 +0,0 @@
-package class9HW;
-
-public class newNewHWexGit {
-    public static void main(String[] args) {
-        System.out.println("HW goes here");
-    }
-}
